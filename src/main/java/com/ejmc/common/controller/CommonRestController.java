a8846ffd529e8a4718d6beb59156cb2df2474bb2
chosh <racoCSD@gmail.com>
2023-06-05 14:00:29 +0900

    ..

--- src/main/java/com/ejmc/common/controller/CommonRestController.java
+++ src/main/java/com/ejmc/common/controller/CommonRestController.java
@@ -23,13 +23,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
-/*
-	1. GET    - /guest : 모든 데이터를 요청한다.
-	2. POST   - /guest : 데이터를 추가한다.
-	3. GET    - /guest/n : n번째 데이터를 요청한다.
-	4. PUT    - /guest/n : n번째 데이터를 수정한다.
-	5. DELETE - /guest/n : n번째 데이터를 제거한다.
-*/
 
 @Tag(name = "2차 과제", description = "템플릿 API Document")
 @RestController
